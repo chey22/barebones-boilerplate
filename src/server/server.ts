1051139030b3f1cab5daf0368753175de4d24769
Cheyenne Mangum <cheyennemangum@gmail.com>
2020-03-31 20:51:49 -0500

    added missing middleware

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,7 +3,18 @@ import apiRouter from './routes';
 
 const app = express();
 
+//MIDDLEWARES
+
+// // cors middleware
+// app.use(cors());
+
+// static middleware
 app.use(express.static('public'));
+
+// this is the body parser middleware that parses the JSON content that's posted to the API so that we can use the JSON content like a JS object
+app.use(express.json());
+
+// middleware router from apiRouter out of routes in index.js
 app.use(apiRouter);
 
 const port = process.env.PORT || 3000;
